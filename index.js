d73df7c12e8e15d69d7f892371f8dbe85dbdd010
yiqun12 <yeequn.xu@gmail.com>
2024-07-14 14:32:47 -0700

    add mode for 102-mm-wide continuous

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ printPngFile({
     vendorId: 0x04f9,
     productId: 0x209D,
     filename: './sample.png',
-    options: { landscape: false },
+    options: { landscape: false, labelWidth: "62-mm-wide continuous" },//"102-mm-wide continuous"
     compression: { enable: true }
 });
